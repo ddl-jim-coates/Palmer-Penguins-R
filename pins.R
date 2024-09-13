3fffa42ac72ce5a9b925fe4a0331c7cc497eb7f5
Jim Coates <jim.coates@dominodatalab.com>
2024-09-13 09:58:51 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/jim_coates/Palmer_Penguins_R?executionId=66e062154ae6f34ec3787c40

--- pins.R
+++ pins.R
@@ -1,7 +1,7 @@
 library(pins)
 board <- board_folder("../data/Pins")
 
-penguin_data <- read.csv('../data/Pins/penguins.csv', header = FALSE, sep = "\t")
+penguin_data <- read.csv('penguins.csv', header = FALSE, sep = "\t")
 
 board %>% pin_write(penguin_data, versioned = TRUE, type="csv")
 
